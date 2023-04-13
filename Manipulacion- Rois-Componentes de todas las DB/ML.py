5b30222ff72d2cc5e019f7979e25aba75ea9ee73
vhenaoi <vhenao.isaza@gmail.com>
2023-04-12 23:45:44 -0500

    error

--- Manipulacion- Rois-Componentes de todas las DB/ML.py
+++ Manipulacion- Rois-Componentes de todas las DB/ML.py
@@ -1,5 +1,252 @@
-import pandas as pd 
+"""
+Modulo de entrenamiento para diferentes pipelines de ML para el
+entrenamiento utilizando los datasets creados. El flujo es capaz de 
+generalizar en la mayoría de los casos y únicamente es necesario
+realizar modificaciones en la ruta a los datos.
 
-path=r'C:\Users\valec\OneDrive - Universidad de Antioquia\Resultados_Armonizacion_BD' #Cambia dependieron de quien lo corra
-data_roi=pd.read_feather(r'{path}\Datosparaorganizardataframes\Data_complete_roi.feather'.format(path=path))
-data_com=pd.read_feather(r'{path}\Datosparaorganizardataframes\Data_complete_ic.feather'.format(path=path))
+"""
+import os
+import pandas as pd 
+import seaborn as sns                                                   
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, \
+                                    cross_val_score, \
+                                    learning_curve, \
+                                    RandomizedSearchCV, \
+                                    GridSearchCV
+from sklearn.inspection import permutation_importance
+import numpy as np
+from sklearn.svm import SVC
+from sklearn import datasets, metrics
+from sklearn.ensemble import RandomForestClassifier
+import seaborn as sns
+from boruta import BorutaPy
+from tpot import TPOTClassifier
+import dataframe_image as dfi
+from keras.models import Sequential,load_model,model_from_json
+
+
+path_santiago = r'C:\Users\santi\Universidad de Antioquia\VALERIA CADAVID CASTRO - Resultados_Armonizacion_BD'
+path_veronica = r'C:\Users\veroh\OneDrive - Universidad de Antioquia\Articulo análisis longitudinal\Resultados_Armonizacion_BD'
+path_save =r'C:\Users\veroh\OneDrive - Universidad de Antioquia\Articulo análisis longitudinal\Resultados_Armonizacion_BD\Graficos_ML'
+name = 'neuroHarmonize_G2G1'
+path_plot = path_save + '/' + name
+os.makedirs(path_plot,exist_ok=True)
+
+def toJSON(model,path):
+  # serialize model to JSON
+  model_json = model.to_json()
+  with open(path+'.json', "w") as json_file:
+      json_file.write(model_json)
+  # serialize weights to HDF5
+  model.save_weights(path+'.h5')
+  print("Saved model to disk")
+
+def loadModel(path):
+  # load json and create model
+  json_file = open(path+'.json', 'r')
+  loaded_model_json = json_file.read()
+  json_file.close()
+  loaded_model = model_from_json(loaded_model_json)
+  # load weights into new model
+  loaded_model.load_weights(path+'.h5')
+  print("Loaded model from disk")
+  return loaded_model
+
+path = path_veronica
+
+modelos = {}
+acc_per_feature = []
+std_per_feature = []
+
+data = pd.read_feather(r'{path}\Datosparaorganizardataframes\Data_complete_ic_neuroHarmonize_G2G1.feather'
+                       .format(path=path))
+
+print(data)
+print(f'sujetos: {data.shape[0]} | caracteristicas: {data.shape[1]}')
+
+## Preprocesamiento
+for group in data['group'].unique():
+    print('{} : {}'.format(group, (data['group']==group).sum()))
+data.describe()
+data.groupby(by='group').describe().T
+
+# eliminación de columnas con datos faltantes
+
+for column in data.columns:
+
+    if data[column].isna().sum() != 0:
+
+        print('{} : {}'.format(column, (data[column].isna().sum())))
+        data.drop(column, axis=1, inplace=True)
+
+print(f'sujetos: {data.shape[0]} | caracteristicas: {data.shape[1]}')
+
+# Se mapean las clases 
+
+clases_mapeadas = {label:idx for idx,label  
+                   in enumerate(np.unique(data['group']))}
+
+data.loc[:,'group'] = data.loc[:,'group'].map(clases_mapeadas) 
+print(clases_mapeadas)
+
+print(data.dtypes.unique())
+data.select_dtypes('O')
+data.groupby(by='sex').describe().T
+
+sexo_mapeado = {label:idx for idx,label
+                in enumerate(np.unique(data['sex']))}
+
+data.loc[:,'sex'] = data.loc[:,'sex'].map(sexo_mapeado) 
+print(sexo_mapeado)
+
+# data pasa a ser el arreglo únicamente con los datos númericos
+
+numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+
+data = data.select_dtypes(include=numerics)
+
+data.shape
+
+# Se elimina la columna, para ponerla al final
+
+target = data.pop('group')
+data.insert(len(data.columns), target.name, target)
+
+## Selección de caracteristicas
+X = data.values[:,:-1]
+y = data.values[:,-1]
+print(X.shape)
+print(y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(                            
+    X, # Valores de X
+    y, # Valores de Y
+    test_size=0.2, # Test de 20%
+    random_state=1, # Semilla
+    stratify=data.values[:,-1]) # que se mantenga la proporcion en la división
+
+# Correlación
+
+
+sns.heatmap(data.corr())
+plt.savefig(path_plot+'/'+'correlation.png')
+
+# Arboles de decisión
+n_estimators = [int(x) for x in np.linspace(start = 100, stop = 2000, num = 30)]
+max_features = ['auto', 'sqrt']
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+min_samples_split = [2, 5, 10]
+min_samples_leaf = [1, 2, 4]
+bootstrap = [True, False]
+criterion = ['gini',  'entropy', 'log_loss']
+
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap,
+               'criterion': criterion
+               }
+forestclf_grid = RandomForestClassifier()
+
+rf_random = RandomizedSearchCV(
+                              estimator=forestclf_grid,
+                              param_distributions=random_grid,
+                              n_iter=100,
+                              cv=10,
+                              verbose=2,
+                              random_state=10,
+                              n_jobs=-1
+                              )
+
+rf_random.fit(X_train, y_train)
+
+best_selected = rf_random.best_estimator_
+GS_fitted = best_selected.fit(X_train, y_train)
+modelos['GridSerach'] = GS_fitted
+
+predicted = GS_fitted.predict(X_test)
+
+print(
+    f"Classification report for classifier {GS_fitted}:\n"
+    f"{metrics.classification_report(y_test, predicted)}\n"
+    )
+
+dataframe_metrics = metrics.classification_report(y_test, predicted, output_dict=True)
+dataframe_metrics = pd.DataFrame(dataframe_metrics).T
+dfi.export(dataframe_metrics, path_plot+'/'+'metrics.png')
+
+
+scores = cross_val_score(
+                        estimator=GS_fitted,
+                        X=X_train,
+                        y=y_train,
+                        cv=10,
+                        n_jobs=-1
+                        )
+print('CV accuracy scores: %s' % scores)
+
+print('\nCV accuracy: %.3f +/- %.3f' %
+    (np.mean(scores), np.std(scores)))
+
+acc_per_feature.append(np.mean(scores))
+std_per_feature.append(np.std(scores))
+train_sizes, train_scores, test_scores = \
+    learning_curve(
+                  estimator=GS_fitted,
+                  X=X_train,
+                  y=y_train,
+                  train_sizes=np.linspace(0.1, 1, 10),
+                  cv=10,
+                  n_jobs=-1
+                  )
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(
+        train_sizes,
+        train_mean,
+        color='blue',
+        marker='o',
+        markersize=5,
+        label='training accuracy'
+        )
+
+plt.fill_between(
+                train_sizes,
+                train_mean + train_std,
+                train_mean - train_std,
+                alpha=0.15,
+                color='blue'
+                )
+
+plt.plot(
+        train_sizes,
+        test_mean,
+        color='green',
+        linestyle='--',
+        marker='s',
+        markersize=5,
+        label='validation accuracy'
+        )
+
+plt.fill_between(
+                train_sizes,
+                test_mean + test_std,
+                test_mean - test_std,
+                alpha=0.15,
+                color='green'
+                )
+
+plt.grid()
+plt.xlabel('number of training samples')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim([0.5, 1.0])
+plt.savefig(path_plot+'/'+'validation_GridSearch.png')
+plt.show()
